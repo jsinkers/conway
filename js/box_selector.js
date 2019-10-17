3c745e014e5f3750dc63a679eff7b1cbef1019ba
James Sinclair <sinclair.j@gmail.com>
2019-10-18 01:34:55 +1100

    tidy up. doesn't have aspect ratio

--- js/box_selector.js
+++ js/box_selector.js
@@ -1,5 +1,3 @@
-//import("helpers.js");
-
 document.addEventListener('DOMContentLoaded', () => {
     console.log(`DOMContentLoaded`);
     // determine window size
@@ -16,6 +14,14 @@ document.addEventListener('DOMContentLoaded', () => {
     var width = 0;
     var height = 0;
     var ratio = 0.8;
+    var mouse = {
+        x: 0,
+        y: 0,
+        startX: 0,
+        startY: 0
+    };
+    var element = null;
+
 
     if (viewportHeight*dy <= viewportWidth*dx) {
         width = Math.round(ratio * viewportWidth);
@@ -31,16 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
         .attr('height', height)
         .attr('id', 'canvas')
         .node();
-
-    var canvasWidth = canvas.width;
-    var canvasHeight = canvas.height;
-    var ctx = canvas.getContext('2d');
-    //var imageData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-
-    var x = 0;
-    var y = 0;
-
-    //initDraw(document.getElementById('canvas'));
 
     function getWidth() {
       return Math.min(
@@ -61,6 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
         document.documentElement.clientHeight
       );
     }
+
     document.getElementById("btnClear").addEventListener("click", () => {
         //disable render button
         document.getElementById("btnRender").disabled = true;
@@ -72,18 +69,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-
-
-    var mouse = {
-        x: 0,
-        y: 0,
-        startX: 0,
-        startY: 0
-    };
-
-    var element = null;
-
-    //const canvas = document.querySelector('#canvas');
     canvas.addEventListener('mousedown', function(e) {
         getCursorPosition(canvas, e);
         if (element !== null) {
@@ -129,74 +114,10 @@ document.addEventListener('DOMContentLoaded', () => {
         const rect = canvas.getBoundingClientRect();
         mouse.x = event.clientX - rect.left;
         mouse.y = event.clientY - rect.top;
-        console.log("x: " + mouse.x + " y: " + mouse.y);
+        //console.log("x: " + mouse.x + " y: " + mouse.y);
     }
-
 
 });
-
-/*function initDraw(canvas) {
-    function setMousePosition(e) {
-        var ev = e;
-        if (ev.pageX) { //Moz
-            mouse.x = ev.pageX + window.pageXOffset;
-            mouse.y = ev.pageY + window.pageYOffset;
-        } else if (ev.clientX) { //IE
-            mouse.x = ev.clientX + document.body.scrollLeft;
-            mouse.y = ev.clientY + document.body.scrollTop;
-        }
-    }
-
-    var mouse = {
-        x: 0,
-        y: 0,
-        startX: 0,
-        startY: 0
-    };
-    var element = null;
-
-    canvas.onmousemove = function (e) {
-        setMousePosition(e);
-        if (element !== null) {
-            element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-            element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-        }
-    };
-
-    canvas.onclick = function (e) {
-        if (element !== null) {
-            element = null;
-            canvas.style.cursor = "default";
-            console.log("finish drawing");
-            boxDrawn();
-        } else {
-            //disable render button
-            document.getElementById("btnRender").disabled = true;
-            document.getElementById("btnClear").disabled = true;
-            // delete existing box if there is one
-            var rect = document.querySelector(".rectangle");
-            if (rect) {
-                rect.remove();
-            }
-            console.log("begin drawing");
-            mouse.x = mouse.x - document.getElementById('canvas').offsetLeft;
-            mouse.startX = mouse.x;
-            mouse.startY = mouse.y;
-            console.log(`x${mouse.x}, y${mouse.y}`);
-            element = document.createElement('div');
-            element.className = 'rectangle';
-            element.style.left = `${mouse.x}px}`;
-            element.style.top = mouse.y + 'px';
-            document.getElementById("container").appendChild(element);
-            canvas.style.cursor = "crosshair";
-        }
-    }
-}*/
-
-
-
 
 function boxDrawn() {
     // enable render button
@@ -204,7 +125,6 @@ function boxDrawn() {
     document.getElementById("btnClear").disabled = false;
 
     // determine (x,y)|(min, max) for the box
-
 
 }
 
